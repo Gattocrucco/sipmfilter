c055fa8e0449381b9a2d60e7c9b042919ddbb26b
Giacomo Petrillo <info@giacomopetrillo.com>
2020-11-02 11:29:16 +0100

    use lower suboptimal tau in fingersnr.py

--- fingersnr.py
+++ fingersnr.py
@@ -10,16 +10,22 @@ import integrate
 filename = 'nuvhd_lf_3x_tile57_77K_64V_6VoV_1.wav'
 data = readwav.readwav(filename, mmap=False)
 
-delta_rel = np.linspace(0.6, 1.4, 10)
-tau = np.linspace(500, 2000, 10)
+tau = np.array([32, 64, 128, 192, 256, 320, 384, 512, 768, 1024])
 
-delta = (tau.reshape(-1, 1) * delta_rel).reshape(-1)
-tau = np.broadcast_to(tau.reshape(-1, 1), tau.shape + delta_rel.shape).reshape(-1)
+# delta for moving average
+delta_rel = np.linspace(0.5, 1.4, 10)
+delta_off = 80 + np.linspace(-40, 40, 10)
+delta = delta_off + delta_rel * tau.reshape(-1, 1)
+delta = np.array(delta, int).reshape(-1)
 
-delta = np.array(delta, int)
+# delta for exponential moving average
+delta_rel_exp = np.linspace(0.1, 2, 10)
+delta_off_exp = np.linspace(75, 500, 10)
+delta_exp = delta_off_exp + delta_rel_exp * tau.reshape(-1, 1)
+delta_exp = np.array(delta_exp, int).reshape(-1)
+
 tau = np.array(tau, int)
-delta_exp = np.array(np.linspace(300, 1500, 10), int)
-delta_exp = np.broadcast_to(delta_exp.reshape(1, -1), (10, 10)).reshape(-1)
+tau = np.broadcast_to(tau.reshape(-1, 1), tau.shape + delta_rel.shape).reshape(-1)
 
 print('computing...')
 start, baseline, vma, vexp = integrate.filter(data, delta, tau, delta_exp, tau)
@@ -30,33 +36,62 @@ ignore = np.any((0 <= baseline_zone) & (baseline_zone < 700), axis=-1)
 print(f'ignoring {np.sum(ignore)} events with values < 700 in baseline zone')
 
 def single_filter_analysis(corr_value, fig1=None, fig2=None):
-    counts, bins = np.histogram(corr_value, bins=500)
-    peaks, pp = signal.find_peaks(counts, prominence=16, height=16, distance=2)
+    """
+    Parameters
+    ----------
+    corr_value : 1D array
+        The filter output already corrected for sign and baseline.
+    fig1, fig2 : matplotlib figure objects (optional)
+        If given, make a fingerplot and a plot of the peak centers and widths.
+    
+    Returns
+    -------
+    snr : float
+        The ratio of the center of the second peak over the width of the first.
+    """
+    
+    # Make a histogram and find the peaks in the histogram.
+    L, R = np.quantile(corr_value, [0, 0.98])
+    bins = np.linspace(L, R, 101)
+    counts, _ = np.histogram(corr_value, bins=bins)
+    peaks, pp = signal.find_peaks(counts, prominence=16, height=16, distance=6)
     ph = pp['peak_heights']
     psel = np.concatenate([[True], (ph[1:] / ph[:-1]) > 1/5])
     peaks = peaks[psel]
     ph = ph[psel]
     if len(peaks) <= 1:
         return 0
-
+    
+    # Take regions around the peaks.
     bins_center = (bins[1:] + bins[:-1]) / 2
     peaks_loc = bins_center[peaks]
     window_mid = (peaks_loc[1:] + peaks_loc[:-1]) / 2
     window = np.concatenate([
-        [peaks_loc[0] - (window_mid[0] - peaks_loc[0])],
+        # [peaks_loc[0] - 2 * (window_mid[0] - peaks_loc[0])],
+        [-np.inf],
         window_mid,
         [peaks_loc[-1] + (peaks_loc[-1] - window_mid[-1])]
     ])
-
-    center, width = np.empty((2, len(window) - 1))
+    
+    # Compute median and interquantile range for each region.
+    center, width, N = np.empty((3, len(window) - 1))
     for i in range(len(window) - 1):
         selection = (window[i] <= corr_value) & (corr_value < window[i + 1])
         values = corr_value[selection]
         center[i] = np.median(values)
         width[i] = np.diff(np.quantile(values, [0.50 - 0.68/2, 0.50 + 0.68/2]))[0] / 2
+        N[i] = len(values)
     
+    # Check if the positions of the peaks make sense.
+    medianstd = np.sqrt(np.pi / 2) * width / np.sqrt(N)
+    firstbad = np.abs(center[0]) > 5 * medianstd[0]
+    secondbad = center[1] < 5 * medianstd[1]
+    bad = firstbad or secondbad
+    
+    # Compute signal to noise ratio.
     snr = center[1] / width[0]
     
+    # Figure of histogram with peaks highlighted.
     if fig1 is not None:
         ax = fig1.subplots(1, 1)
         
@@ -80,10 +115,11 @@ def single_filter_analysis(corr_value, fig1=None, fig2=None):
             ax.axvline(window[i], **kwvline)
             kwvline.pop('label', None)
 
-        ax.set_xlim(window[0] - 10, window[-1] + 10)
+        ax.set_xlim(L - 10, R + 10)
         ax.set_ylim(0, ax.get_ylim()[1])
         ax.legend(loc='upper right')
     
+    # Figure of centers and widths of peaks.
     if fig2 is not None:
         ax = fig2.subplots(2, 1, sharex=True)
         
@@ -98,7 +134,7 @@ def single_filter_analysis(corr_value, fig1=None, fig2=None):
         for a in ax:
             a.grid()
     
-    return snr
+    return 0 if bad else snr
 
 snr = np.empty((2, len(tau)))
 
@@ -132,7 +168,7 @@ axs[1].set_xlabel('Offset from trigger [samples]')
 
 for i, ax in enumerate(axs):
     for j in range(snr.shape[1]):
-        alpha = (j + 1) / snr.shape[1]
+        alpha = 1 - (j / snr.shape[1])
         label = f'tau = {tau[j, 0]}'
         d = delta if i == 0 else delta_exp
         ax.plot(d[j], snr[i, j], color='black', alpha=alpha, label=label)
